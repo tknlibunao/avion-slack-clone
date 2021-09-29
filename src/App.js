0182b9041b7a398c68813651c663f8e54fe7fba0
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-09-29 19:14:18 +0800

    APIfunctions #7: modified the routing such that root renders Home and signup/login redirects to Chat Room on success

--- src/App.js
+++ src/App.js
@@ -10,26 +10,47 @@ import {
 	// useParams,
 } from 'react-router-dom';
 
-// import Home from './components/Home';
-// import Register from './components/Register';
 import Loading from './components/Loading/Loading';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Chat from './components/Chat/Chat';
-import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+// import Login from './components/Login/Login';
+// import Register from './components/Login/Register';
 import styled from 'styled-components';
 
 function App() {
-	const [success, setSuccess] = useState(false);
+	/* HEADER PARAMETERS */
 	const [accessToken, setAccessToken] = useState('');
 	const [client, setClient] = useState('');
 	const [expiry, setExpiry] = useState('');
 	const [uid, setUID] = useState('');
-	const [channelsList, setChannelsList] = useState([]);
+
+	/* USER PARAMETERS */
+	// const [email, setEmail] = useState('');
+	// const [password, setPassword] = useState('');
+	// const [confirmation, setConfirmation] = useState('');
+
+	/* API DATA */
+	// const [channelsList, setChannelsList] = useState([]);
+	// const [usersList, setUsersList] = useState([]);
 	const [myChannels, setMyChannels] = useState([]);
-	const [usersList, setUsersList] = useState([]);
-	const [DMList, setDMList] = useState([]);
+	// const [DMList, setDMList] = useState([]);
+
+	/*  FLAGS */
+	const [success, setSuccess] = useState(false);
 	const [newChannel, setNewChannel] = useState(0);
+
+	/* Define URL */
+	const url = 'http://206.189.91.54//api/v1';
+
+	/* Initialize Headers */
+	var myHeaders = new Headers();
+	myHeaders.append('Content-Type', 'application/json');
+	myHeaders.append('access-token', `${accessToken}`);
+	myHeaders.append('client', `${client}`);
+	myHeaders.append('expiry', `${expiry}`);
+	myHeaders.append('uid', `${uid}`);
 
 	// const registerUser = () => {
 	// 	let myHeaders = new Headers();
@@ -57,12 +78,9 @@ function App() {
 	// };
 
 	const loginUser = () => {
-		let myHeaders = new Headers();
-		myHeaders.append('Content-Type', 'application/json');
-
 		let raw = JSON.stringify({
-			// email: 'user2@example.com',
-			email: 'usersample03@gmail.com',
+			email: 'user2@example.com',
+			// email: 'usersample03@gmail.com',
 			password: '12345678',
 		});
 
@@ -73,7 +91,7 @@ function App() {
 			redirect: 'follow',
 		};
 
-		fetch('http://206.189.91.54//api/v1/auth/sign_in', requestOptions)
+		fetch(`${url}/auth/sign_in`, requestOptions)
 			.then((response) => {
 				response.headers.forEach((item, key) => {
 					switch (key) {
@@ -94,110 +112,21 @@ function App() {
 					}
 				});
 				if (response.status === 200) {
-					console.log('LOGIN SUCCESS');
-					setSuccess(true);
-					// console.log(success);
+					// console.log('LOGIN SUCCESS');
+					// setSuccess(true);
 				}
 			})
 			.catch((error) => console.log('error', error));
 	};
 
-	const getUsers = () => {
-		var myHeaders = new Headers();
-		myHeaders.append('access-token', `${accessToken}`);
-		myHeaders.append('client', `${client}`);
-		myHeaders.append('expiry', `${expiry}`);
-		myHeaders.append('uid', `${uid}`);
-
-		var requestOptions = {
-			method: 'GET',
-			headers: myHeaders,
-			redirect: 'follow',
-		};
-
-		fetch('http://206.189.91.54//api/v1/users', requestOptions)
-			.then((response) => response.json())
-			.then((result) => {
-				let updatedList = [];
-				result.data.forEach((item) => {
-					updatedList.push({
-						name: item.name,
-						id: item.id,
-						uid: item.uid,
-					});
-				});
-				setUsersList(updatedList);
-			})
-			.catch((error) => console.log('error', error));
-	};
-
-	const getDMs = () => {
-		var myHeaders = new Headers();
-		myHeaders.append('access-token', `${accessToken}`);
-		myHeaders.append('client', `${client}`);
-		myHeaders.append('expiry', `${expiry}`);
-		myHeaders.append('uid', `${uid}`);
-
-		var requestOptions = {
-			method: 'GET',
-			headers: myHeaders,
-			redirect: 'follow',
-		};
-
-		fetch('http://206.189.91.54//api/v1/users/recent', requestOptions)
-			.then((response) => response.json())
-			.then((result) => {
-				let updatedList = [];
-				result.data.forEach((item) => {
-					updatedList.push({
-						id: item.id,
-						uid: item.uid,
-					});
-				});
-				setDMList(updatedList);
-			})
-			.catch((error) => console.log('error', error));
-	};
-
-	const retrieveMessage = () => {
-		var myHeaders = new Headers();
-		myHeaders.append('access-token', `${accessToken}`);
-		myHeaders.append('client', `${client}`);
-		myHeaders.append('expiry', `${expiry}`);
-		myHeaders.append('uid', `${uid}`);
-
-		var requestOptions = {
-			method: 'GET',
-			headers: myHeaders,
-			redirect: 'follow',
-		};
-
-		// receiver_id: check id from recently DMs
-		// NOTE: receiver_id can also be the sender of the message to our user
-		// check the result.data.forEach(item.[sender/receiver].uid) to see who is sender/receiver
-		fetch(
-			'http://206.189.91.54//api/v1/messages?receiver_class=User&receiver_id=496',
-			requestOptions
-		)
-			.then((response) => response.json())
-			.then((result) => console.log(result.data))
-			.catch((error) => console.log('error', error));
-	};
-
 	const getMyChannels = () => {
-		var myHeaders = new Headers();
-		myHeaders.append('access-token', `${accessToken}`);
-		myHeaders.append('client', `${client}`);
-		myHeaders.append('expiry', `${expiry}`);
-		myHeaders.append('uid', `${uid}`);
-
 		var requestOptions = {
 			method: 'GET',
 			headers: myHeaders,
 			redirect: 'follow',
 		};
 
-		fetch('http://206.189.91.54//api/v1/channel/owned', requestOptions)
+		fetch(`${url}/channel/owned`, requestOptions)
 			.then((response) => response.json())
 			.then((result) => {
 				let updatedList = [];
@@ -205,78 +134,30 @@ function App() {
 					updatedList.push({
 						name: item.name,
 						id: item.id,
+						created_at: item.created_at,
 					});
 				});
-				setMyChannels(updatedList);
+				setMyChannels(sortList(updatedList));
 			})
 			.catch((error) => console.log('error', error));
 	};
 
-	const getAllChannels = () => {
-		let myHeaders = new Headers();
-		myHeaders.append('access-token', `${accessToken}`);
-		myHeaders.append('client', `${client}`);
-		myHeaders.append('expiry', `${expiry}`);
-		myHeaders.append('uid', `${uid}`);
-
-		let requestOptions = {
-			method: 'GET',
-			headers: myHeaders,
-			redirect: 'follow',
-		};
-
-		fetch('http://206.189.91.54//api/v1/channels', requestOptions)
-			.then((response) => response.json())
-			.then((result) => {
-				let updatedList = [];
-				result.data.forEach((item) => {
-					updatedList.push({
-						name: item.name,
-						id: item.id,
-						owner_id: item.owner_id,
-					});
-				});
-				setChannelsList(updatedList);
-			})
-			.catch((error) => console.log('error', error));
-	};
-
-	const selectChannel = () => {
-		var myHeaders = new Headers();
-		myHeaders.append('access-token', `${accessToken}`);
-		myHeaders.append('client', `${client}`);
-		myHeaders.append('expiry', `${expiry}`);
-		myHeaders.append('uid', `${uid}`);
-
-		var requestOptions = {
-			method: 'GET',
-			headers: myHeaders,
-			redirect: 'follow',
-		};
-
-		fetch('http://206.189.91.54//api/v1/channels/676', requestOptions)
-			.then((response) => response.json())
-			.then((result) => console.log(result))
-			.catch((error) => console.log('error', error));
+	const sortList = (list) => {
+		return list.sort(function (a, b) {
+			if (a.created_at < b.created_at) return -1;
+			if (a.created_at > b.created_at) return 1;
+			return 0;
+		});
 	};
 
 	const createNewChannel = () => {
-		var newChannel = prompt('Create new channel');
-
-		// console.log('New channel created: ', newChannel);
-		var myHeaders = new Headers();
-		myHeaders.append('Content-Type', 'application/json');
-		myHeaders.append('access-token', `${accessToken}`);
-		myHeaders.append('client', `${client}`);
-		myHeaders.append('expiry', `${expiry}`);
-		myHeaders.append('uid', `${uid}`);
+		var channelName = prompt('Create new channel');
 
 		var raw = JSON.stringify({
-			name: newChannel,
+			name: channelName,
 			user_ids: [],
 		});
 
-		console.log(raw);
 		var requestOptions = {
 			method: 'POST',
 			headers: myHeaders,
@@ -284,43 +165,174 @@ function App() {
 			redirect: 'follow',
 		};
 
-		fetch('http://206.189.91.54//api/v1/channels', requestOptions)
+		fetch(`${url}/channels`, requestOptions)
 			.then((response) => response.text())
 			.then((result) => console.log(result))
 			.catch((error) => console.log('error', error));
 
 		setNewChannel((newChannel) => newChannel + 1);
+		getMyChannels();
 	};
 
-	const addMember = () => {
-		var myHeaders = new Headers();
-		myHeaders.append('Content-Type', 'application/json');
-		myHeaders.append('access-token', `${accessToken}`);
-		myHeaders.append('client', `${client}`);
-		myHeaders.append('expiry', `${expiry}`);
-		myHeaders.append('uid', `${uid}`);
+	// const getAllUsers = () => {
+	// 	var requestOptions = {
+	// 		method: 'GET',
+	// 		headers: myHeaders,
+	// 		redirect: 'follow',
+	// 	};
 
-		var raw = JSON.stringify({
-			id: 676,
-			member_id: 1,
-		});
+	// 	fetch('http://206.189.91.54//api/v1/users', requestOptions)
+	// 		.then((response) => response.json())
+	// 		.then((result) => {
+	// 			let updatedList = [];
+	// 			result.data.forEach((item) => {
+	// 				updatedList.push({
+	// 					name: item.name,
+	// 					id: item.id,
+	// 					uid: item.uid,
+	// 				});
+	// 			});
+	// 			setUsersList(updatedList);
+	// 		})
+	// 		.catch((error) => console.log('error', error));
+	// };
 
-		var requestOptions = {
-			method: 'POST',
-			headers: myHeaders,
-			body: raw,
-			redirect: 'follow',
-		};
+	// const getDMs = () => {
+	// 	var myHeaders = new Headers();
+	// 	myHeaders.append('access-token', `${accessToken}`);
+	// 	myHeaders.append('client', `${client}`);
+	// 	myHeaders.append('expiry', `${expiry}`);
+	// 	myHeaders.append('uid', `${uid}`);
 
-		fetch('http://206.189.91.54//api/v1/channel/add_member', requestOptions)
-			.then((response) => response.text())
-			.then((result) => console.log(result))
-			.catch((error) => console.log('error', error));
-	};
+	// 	var requestOptions = {
+	// 		method: 'GET',
+	// 		headers: myHeaders,
+	// 		redirect: 'follow',
+	// 	};
+
+	// 	fetch('http://206.189.91.54//api/v1/users/recent', requestOptions)
+	// 		.then((response) => response.json())
+	// 		.then((result) => {
+	// 			let updatedList = [];
+	// 			result.data.forEach((item) => {
+	// 				updatedList.push({
+	// 					id: item.id,
+	// 					uid: item.uid,
+	// 				});
+	// 			});
+	// 			setDMList(updatedList);
+	// 		})
+	// 		.catch((error) => console.log('error', error));
+	// };
+
+	// const retrieveMessage = () => {
+	// 	var myHeaders = new Headers();
+	// 	myHeaders.append('access-token', `${accessToken}`);
+	// 	myHeaders.append('client', `${client}`);
+	// 	myHeaders.append('expiry', `${expiry}`);
+	// 	myHeaders.append('uid', `${uid}`);
+
+	// 	var requestOptions = {
+	// 		method: 'GET',
+	// 		headers: myHeaders,
+	// 		redirect: 'follow',
+	// 	};
+
+	// 	// receiver_id: check id from recently DMs
+	// 	// NOTE: receiver_id can also be the sender of the message to our user
+	// 	// check the result.data.forEach(item.[sender/receiver].uid) to see who is sender/receiver
+
+	// 	// receiver_class: [Channel/User]
+	// 	fetch(
+	// 		'http://206.189.91.54//api/v1/messages?receiver_class=User&receiver_id=496',
+	// 		requestOptions
+	// 	)
+	// 		.then((response) => response.json())
+	// 		.then((result) => console.log(result.data))
+	// 		.catch((error) => console.log('error', error));
+	// };
+
+	// const getAllChannels = () => {
+	// 	let myHeaders = new Headers();
+	// 	myHeaders.append('access-token', `${accessToken}`);
+	// 	myHeaders.append('client', `${client}`);
+	// 	myHeaders.append('expiry', `${expiry}`);
+	// 	myHeaders.append('uid', `${uid}`);
+
+	// 	let requestOptions = {
+	// 		method: 'GET',
+	// 		headers: myHeaders,
+	// 		redirect: 'follow',
+	// 	};
+
+	// 	fetch('http://206.189.91.54//api/v1/channels', requestOptions)
+	// 		.then((response) => response.json())
+	// 		.then((result) => {
+	// 			let updatedList = [];
+	// 			result.data.forEach((item) => {
+	// 				updatedList.push({
+	// 					name: item.name,
+	// 					id: item.id,
+	// 					owner_id: item.owner_id,
+	// 				});
+	// 			});
+	// 			setChannelsList(updatedList);
+	// 		})
+	// 		.catch((error) => console.log('error', error));
+	// };
+
+	// const selectChannel = () => {
+	// 	var myHeaders = new Headers();
+	// 	myHeaders.append('access-token', `${accessToken}`);
+	// 	myHeaders.append('client', `${client}`);
+	// 	myHeaders.append('expiry', `${expiry}`);
+	// 	myHeaders.append('uid', `${uid}`);
+
+	// 	var requestOptions = {
+	// 		method: 'GET',
+	// 		headers: myHeaders,
+	// 		redirect: 'follow',
+	// 	};
+
+	// 	fetch('http://206.189.91.54//api/v1/channels/676', requestOptions)
+	// 		.then((response) => response.json())
+	// 		.then((result) => console.log(result))
+	// 		.catch((error) => console.log('error', error));
+	// };
+
+	// const addMember = () => {
+	// 	var myHeaders = new Headers();
+	// 	myHeaders.append('Content-Type', 'application/json');
+	// 	myHeaders.append('access-token', `${accessToken}`);
+	// 	myHeaders.append('client', `${client}`);
+	// 	myHeaders.append('expiry', `${expiry}`);
+	// 	myHeaders.append('uid', `${uid}`);
+
+	// 	var raw = JSON.stringify({
+	// 		id: 676,
+	// 		member_id: 1,
+	// 	});
+
+	// 	var requestOptions = {
+	// 		method: 'POST',
+	// 		headers: myHeaders,
+	// 		body: raw,
+	// 		redirect: 'follow',
+	// 	};
+
+	// 	fetch('http://206.189.91.54//api/v1/channel/add_member', requestOptions)
+	// 		.then((response) => response.text())
+	// 		.then((result) => console.log(result))
+	// 		.catch((error) => console.log('error', error));
+	// };
 
 	useEffect(() => {
 		loginUser();
-	}, []);
+	}, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+	useEffect(() => {
+		getMyChannels();
+	}, [success, newChannel]); // eslint-disable-line react-hooks/exhaustive-deps
 
 	// useEffect(() => {
 	// 	if (success) {
@@ -330,7 +342,7 @@ function App() {
 	// }, [success]); // eslint-disable-line react-hooks/exhaustive-deps
 
 	// useEffect(() => {
-	// 	if (success) getUsers();
+	// 	if (success) getAllUsers();
 	// }, [success]); // eslint-disable-line react-hooks/exhaustive-deps
 
 	// useEffect(() => {
@@ -341,13 +353,9 @@ function App() {
 	// 	retrieveMessage();
 	// }, [success]); // eslint-disable-line react-hooks/exhaustive-deps
 
-	useEffect(() => {
-		getMyChannels();
-	}, [success, newChannel]); // eslint-disable-line react-hooks/exhaustive-deps
-
-	useEffect(() => {
-		selectChannel();
-	}, [success]); // eslint-disable-line react-hooks/exhaustive-deps
+	// useEffect(() => {
+	// 	selectChannel();
+	// }, [success]); // eslint-disable-line react-hooks/exhaustive-deps
 
 	useEffect(() => {
 		console.log(myChannels);
@@ -390,11 +398,14 @@ function App() {
 								<Loading />
 							)}
 						</Route>
+						<Route path='/signup'>
+							{success ? <Redirect to='/room' /> : <Register />}
+						</Route>
 						<Route path='/login'>
-							<Login />
+							{success ? <Redirect to='/room' /> : <Login />}
 						</Route>
 						<Route path='/'>
-							{success ? <Redirect to='/room' /> : <Login />}
+							{success ? <Redirect to='/room' /> : <Home />}
 						</Route>
 					</Switch>
 				</Router>
@@ -417,3 +428,13 @@ const Main = styled.div`
 	display: grid;
 	grid-template-columns: 260px auto;
 `;
+
+const Login = () => {
+	return <h1>Login Form</h1>;
+};
+
+const Register = () => {
+	return (
+		<h1>Register Form</h1>
+	)
+}
