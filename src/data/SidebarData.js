9e3906fafbf6d69d4b7d9f16a27ab8cfe46acf53
Billy Fortier <billyfortier12901@gmail.com>
2021-09-29 23:27:19 +0800

    Created data folder for mapping of data

---
+++ src/data/SidebarData.js
@@ -0,0 +1,21 @@
+export const sidebarItems = [
+    {
+        icon: <box-icon name='message-rounded-detail' flip='horizontal' color='var(--sidebar-font-color)' ></box-icon>,
+        text: <span>Threads</span>
+    },
+
+    {
+        icon: <box-icon name='conversation' flip='horizontal' color='var(--sidebar-font-color)' ></box-icon>,
+        text: <span>All DMs</span>
+    },
+
+    {
+        icon: <box-icon name='at' color='var(--sidebar-font-color)' ></box-icon>,
+        text: <span>Mentions & reactions</span>
+    },
+
+    {
+        icon: <box-icon name='dots-vertical-rounded' color='var(--sidebar-font-color)' ></box-icon>,
+        text: <span>More</span>
+    },
+]
