7fdc1403ce89d1c67594d6be780116fcb4ad4558
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-09-29 18:56:50 +0800

    APIfunctions #7: modified Chat component to have ChatInput and ChatMessage child components

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,7 +1,69 @@
 import React from 'react';
+import styled from 'styled-components';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import ChatInput from './ChatInput';
+import ChatMessage from './ChatMessage';
 
 function Chat() {
-	return <div>Chat</div>;
+	return (
+		<Container>
+			<Header>
+				<Channel>
+					<ChannelName>#ChannelName</ChannelName>
+					<ChannelInfo>
+						Company-wide announcements and work-based matters
+					</ChannelInfo>
+				</Channel>
+				<ChannelDetails>
+					<div>Details</div>
+					<Info />
+				</ChannelDetails>
+			</Header>
+			<MessageContainer>
+				<ChatMessage />
+			</MessageContainer>
+			<ChatInput />
+		</Container>
+	);
 }
 
 export default Chat;
+
+const Container = styled.div`
+	display: grid;
+	grid-template-rows: 64px auto min-content;
+`;
+
+const Channel = styled.div``;
+
+const ChannelDetails = styled.div`
+	display: flex;
+	align-items: center;
+	color: #606060;
+`;
+
+const ChannelName = styled.div`
+	font-weight: 700;
+`;
+
+const ChannelInfo = styled.div`
+	font-weight: 400;
+	color: #606060;
+	font-size: 13px;
+	margin-top: 8px;
+`;
+
+const Info = styled(InfoOutlinedIcon)`
+	margin-left: 10px;
+`;
+
+const Header = styled.div`
+	padding-left: 20px;
+	padding-right: 20px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	border-bottom: 1px solid rgba(83, 39, 83, 0.13);
+`;
+
+const MessageContainer = styled.div``;
