d0494c9a5bb1ca82534d02ca2c7bd6a06e5fb1b8
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-10-01 16:28:54 +0800

    Message #7: comment out unnecessary elements

--- src/data/SidebarData.js
+++ src/data/SidebarData.js
@@ -1,6 +1,6 @@
 import MessageIcon from '@material-ui/icons/Message';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
+// import InboxIcon from '@material-ui/icons/Inbox';
+// import DraftsIcon from '@material-ui/icons/Drafts';
 import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
 import PeopleIcon from '@material-ui/icons/People';
 import AppsIcon from '@material-ui/icons/Apps';
@@ -10,14 +10,14 @@ export const sidebarItemsData = [
 		icon: <MessageIcon />,
 		text: 'Thread',
 	},
-	{
-		icon: <InboxIcon />,
-		text: 'All DMs',
-	},
-	{
-		icon: <DraftsIcon />,
-		text: 'Mentions and Reactions',
-	},
+	// {
+	// 	icon: <InboxIcon />,
+	// 	text: 'All DMs',
+	// },
+	// {
+	// 	icon: <DraftsIcon />,
+	// 	text: 'Mentions and Reactions',
+	// },
 	{
 		icon: <BookmarkBorderIcon />,
 		text: 'Saved Items',
