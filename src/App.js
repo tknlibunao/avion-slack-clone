96d92d11a231899d16db1b157a36a60e93b7b877
jerco2 <jericogodoy7296@gmail.com>
2021-10-02 10:01:39 +0800

    Mainpage #16: added Login component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,111 @@
-import './App.css'
-import styled from 'styled-components'
-import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Chat from './components/Chat/Chat'
-import Sidebar from './components/Sidebar/Sidebar'
-import Login from './components/Login/Login'
-import Header from './components/Header/Header'
+import "./App.css";
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Chat from "./components/Chat/Chat";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Login from "./components/Login/Login";
+import Header from "./components/Header/Header";
 
 function App() {
+  /* USER PARAMETERS */
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmation, setConfirmation] = useState("");
+
+  const [success, setSuccess] = useState(false);
+
+  const inputUser = (e) => {
+    setEmail(e.target.value);
+  };
+  const inputPassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("access-token", localStorage.getItem("token"));
+  myHeaders.append("client", localStorage.getItem("client"));
+  myHeaders.append("expiry", localStorage.getItem("expiry"));
+  myHeaders.append("uid", localStorage.getItem("uid"));
+
+  const url = "http://206.189.91.54//api/v1";
+
+  const loginUser = (e) => {
+    e.preventDefault();
+    let raw = JSON.stringify({
+      email: email,
+      // email: 'usersample03@gmail.com',
+      password: password,
+    });
+
+    let requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch(`${url}/auth/sign_in`, requestOptions)
+      .then((response) => {
+        response.headers.forEach((item, key) => {
+          switch (key) {
+            case "access-token":
+              // setAccessToken(item);
+              localStorage.setItem("token", item);
+              break;
+            case "client":
+              // setClient(item);
+              localStorage.setItem("client", item);
+              break;
+            case "expiry":
+              // setExpiry(item);
+              localStorage.setItem("expiry", item);
+              break;
+            case "uid":
+              // setUID(item);
+              localStorage.setItem("uid", item);
+              break;
+            default:
+              break;
+          }
+        });
+        if (response.status === 200) {
+          // alert("LOGIN SUCCESS");
+          setSuccess(true);
+          // localStorage.setItem("success", success);
+        }
+      })
+      .catch((error) => console.log("error", error));
+  };
+
   return (
     <div className="App">
       <Router>
         <Container>
           <Switch>
-            <Route path='/room'>
+            <Route path="/room">
               <Header />
               <Main>
                 <Sidebar />
                 <Chat />
               </Main>
             </Route>
-            <Route path='/'>
-              <Login />
+            <Route path="/">
+              {localStorage.getItem("token") !== null ? (
+                <Redirect to="/room/:path/:id" />
+              ) : (
+                <Login
+                  onSubmit={loginUser}
+                  inputUser={inputUser}
+                  inputPassword={inputPassword}
+                />
+              )}
             </Route>
           </Switch>
         </Container>
@@ -37,9 +121,9 @@ const Container = styled.div`
   grid-template-rows: 38px auto;
   width: 100vw;
   height: 100vh;
-`
+`;
 const Main = styled.div`
   display: grid;
   grid-template-columns: 260px auto;
   background: var(--chatarea-color);
-`
+`;
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,28 +1,37 @@
 function Input({
-	divClass,
-	label,
-	inputClass,
-	type,
-	name,
-	value,
-	onChange,
-	onClick,
-	autoComplete,
+  divClass,
+  label,
+  inputClass,
+  type,
+  name,
+  value,
+  onChange,
+  onClick,
+  autoComplete,
+  style,
+  readOnly,
 }) {
-	return (
-		<div className={divClass}>
-			<label className='Label'>{label} </label>
-			<input
-				className={inputClass}
-				type={type}
-				name={name}
-				value={value}
-				autoComplete={autoComplete}
-				onChange={(e) => onChange(e)}
-				onClick={onClick}
-			/>
-		</div>
-	);
+  return (
+    <div
+      className={divClass}
+      style={{ display: "flex", flexDirection: "column" }}
+    >
+      <label className="Label" style={{ fontWeight: "bold" }}>
+        {label}{" "}
+      </label>
+      <input
+        className={inputClass}
+        type={type}
+        name={name}
+        value={value}
+        autoComplete={autoComplete}
+        onChange={onChange}
+        onClick={onClick}
+        style={style}
+        readOnly={readOnly}
+      />
+    </div>
+  );
 }
 
 export default Input;
