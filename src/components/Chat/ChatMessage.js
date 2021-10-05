fe272ded74b375dee468a4673c41a3607adab04d
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-10-05 13:10:40 +0800

    Main Page #16: in Chat Message component, added parameters (sender, body, date)

--- src/components/Chat/ChatMessage.js
+++ src/components/Chat/ChatMessage.js
@@ -1,69 +1,64 @@
-import React from 'react'
-import styled from 'styled-components'
-import userDefaultImage from '../../assets/userDefaultImage.png'
+import React from 'react';
+import styled from 'styled-components';
+import userDefaultImage from '../../assets/userDefaultImage.png';
 
-const ChatMessage = () => {
-    return (
-        <Container>
-            <UserAvatar>
-                <img src={userDefaultImage} alt="" />
-            </UserAvatar>
-            <MessageContent>
-                <Name>
-                    Bills
-                    <span>09:00 PM</span>
-                </Name>
-                <Text>
-                    Hellooooo
-                </Text>
-            </MessageContent>
-        </Container>
-    )
-}
+const ChatMessage = ({ sender, body, date }) => {
+	return (
+		<Container>
+			<UserAvatar>
+				<img src={userDefaultImage} alt='' />
+			</UserAvatar>
+			<MessageContent>
+				<Name>
+					{sender}
+					<span>{date.match(/\d\d:\d\d/)}</span>
+				</Name>
+				<Text>{body}</Text>
+			</MessageContent>
+		</Container>
+	);
+};
 
-export default ChatMessage
+export default ChatMessage;
 
 const Container = styled.div`
-    display: flex;
-    align-items: center;
-    padding: 8px 20px;
+	display: flex;
+	align-items: center;
+	padding: 8px 20px;
 
-    :hover {
-        background-color: #F8F8F8;
-    }
-`
+	:hover {
+		background-color: #f8f8f8;
+	}
+`;
 
 const UserAvatar = styled.div`
-    width: 36px;
-    height:36px;
-    border-radius: 4px;
-    overflow: hidden;
-    margin-right: 10px;
+	width: 36px;
+	height: 36px;
+	border-radius: 4px;
+	overflow: hidden;
+	margin-right: 10px;
 
-    img {
-        width: 100%;
-    }
-`
+	img {
+		width: 100%;
+	}
+`;
 
 const MessageContent = styled.div`
-    display: flex;
-    flex-direction: column;
-`
+	display: flex;
+	flex-direction: column;
+`;
 
 const Name = styled.div`
-    font-weight: 900;
-    font-size: 15px;
-    line-height: 1.4;
+	font-weight: 900;
+	font-size: 15px;
+	line-height: 1.4;
 
-    span {
-        font-size: 13px;
-        margin-left: 10px;
-        font-weight: 400;
-        color: #707070;
-    }
-`
+	span {
+		font-size: 13px;
+		margin-left: 10px;
+		font-weight: 400;
+		color: #707070;
+	}
+`;
 
-const Text = styled.div`
-
-`
-
+const Text = styled.div``;
