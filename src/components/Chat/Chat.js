c69d5f05d90824f09b60bd38fa965cb739470911
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-10-06 11:20:08 +0800

    Main Page #16: in Chat component, commented out setMessageList([]) in retrieveMessage() line 122

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,255 +1,255 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router";
-import styled from "styled-components";
-import ChatInput from "./ChatInput";
-import ChatMessage from "./ChatMessage";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+import styled from 'styled-components';
+import ChatInput from './ChatInput';
+import ChatMessage from './ChatMessage';
 
 const Chat = ({ channelsList, DMList, myHeaders, url, usersList }) => {
-  let { path, id } = useParams();
-  const [display, setDisplay] = useState({ id });
-  const [message, setMessage] = useState("");
-  const [messageList, setMessageList] = useState([]);
-  const [channelMembers, setChannelMembers] = useState([]);
-  const [newMember, setNewMember] = useState(0);
+	let { path, id } = useParams();
+	const [display, setDisplay] = useState({ id });
+	const [message, setMessage] = useState('');
+	const [messageList, setMessageList] = useState([]);
+	const [channelMembers, setChannelMembers] = useState([]);
+	const [newMember, setNewMember] = useState(0);
 
-  const getChatDisplay = () => {
-    let items = [];
-    switch (path) {
-      case "channel":
-        items = channelsList;
-        break;
-      case "messages":
-        items = DMList;
-        break;
-      default:
-    }
-    items.forEach((item) => {
-      if (Number(id) === Number(item.id)) {
-        setDisplay(item);
-      }
-    });
-  };
+	const getChatDisplay = () => {
+		let items = [];
+		switch (path) {
+			case 'channel':
+				items = channelsList;
+				break;
+			case 'messages':
+				items = DMList;
+				break;
+			default:
+		}
+		items.forEach((item) => {
+			if (Number(id) === Number(item.id)) {
+				setDisplay(item);
+			}
+		});
+	};
 
-  /* CHANNEL FUNCTIONS */
-  const getChannelDetails = () => {
-    if (path === "messages") return;
-    var requestOptions = {
-      method: "GET",
-      headers: myHeaders,
-      redirect: "follow",
-    };
+	/* CHANNEL FUNCTIONS */
+	const getChannelDetails = () => {
+		if (path === 'messages') return;
+		var requestOptions = {
+			method: 'GET',
+			headers: myHeaders,
+			redirect: 'follow',
+		};
 
-    fetch(`${url}/channels/${id}`, requestOptions)
-      .then((response) => response.json())
-      .then((result) => {
-        // console.log(result.data);
-        let updatedList = [];
-        result.data.channel_members.forEach((item) => {
-          updatedList.push(item.user_id);
-        });
-        setChannelMembers(updatedList);
-        console.log(updatedList);
-      })
-      .catch((error) => console.log("error", error));
-  };
+		fetch(`${url}/channels/${id}`, requestOptions)
+			.then((response) => response.json())
+			.then((result) => {
+				// console.log(result.data);
+				let updatedList = [];
+				result.data.channel_members.forEach((item) => {
+					updatedList.push(item.user_id);
+				});
+				setChannelMembers(updatedList);
+				console.log(updatedList);
+			})
+			.catch((error) => console.log('error', error));
+	};
 
-  const addMember = (channelId) => {
-    let memberId = prompt("Enter member ID:");
-    var raw = JSON.stringify({
-      id: channelId,
-      member_id: memberId,
-    });
+	const addMember = (channelId) => {
+		let memberId = prompt('Enter member ID:');
+		var raw = JSON.stringify({
+			id: channelId,
+			member_id: memberId,
+		});
 
-    var requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: raw,
-      redirect: "follow",
-    };
+		var requestOptions = {
+			method: 'POST',
+			headers: myHeaders,
+			body: raw,
+			redirect: 'follow',
+		};
 
-    fetch(`${url}/channel/add_member`, requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.log("error", error));
+		fetch(`${url}/channel/add_member`, requestOptions)
+			.then((response) => response.text())
+			.then((result) => console.log(result))
+			.catch((error) => console.log('error', error));
 
-    setNewMember((newMember) => newMember + 1);
-    getChannelDetails();
-  };
+		setNewMember((newMember) => newMember + 1);
+		getChannelDetails();
+	};
 
-  /* MESSAGES FUNCTIONS */
-  const inputMessage = (e) => {
-    setMessage(e.target.value);
-  };
+	/* MESSAGES FUNCTIONS */
+	const inputMessage = (e) => {
+		setMessage(e.target.value);
+	};
 
-  const sendMessage = (e) => {
-    e.preventDefault();
+	const sendMessage = (e) => {
+		e.preventDefault();
 
-    if (message === "") {
-      return;
-    } else {
-      let receiver_class = "";
-      switch (path) {
-        case "channel":
-          receiver_class = "Channel";
-          break;
-        case "messages":
-          receiver_class = "User";
-          break;
-        default:
-      }
-      fetch(`${url}/messages`, {
-        method: "POST",
-        body: JSON.stringify({
-          receiver_id: id,
-          receiver_class: receiver_class,
-          body: message,
-        }),
-        headers: myHeaders,
-        redirect: "follow",
-      })
-        .then((res) => {
-          console.log(res);
-          if (res.status === 200) {
-            setMessage("");
-          }
-          retrieveMessage(receiver_class);
-        })
-        .catch((err) => console.log(err));
-    }
-  };
+		if (message === '') {
+			return;
+		} else {
+			let receiver_class = '';
+			switch (path) {
+				case 'channel':
+					receiver_class = 'Channel';
+					break;
+				case 'messages':
+					receiver_class = 'User';
+					break;
+				default:
+			}
+			fetch(`${url}/messages`, {
+				method: 'POST',
+				body: JSON.stringify({
+					receiver_id: id,
+					receiver_class: receiver_class,
+					body: message,
+				}),
+				headers: myHeaders,
+				redirect: 'follow',
+			})
+				.then((res) => {
+					console.log(res);
+					if (res.status === 200) {
+						setMessage('');
+					}
+					retrieveMessage(receiver_class);
+				})
+				.catch((err) => console.log(err));
+		}
+	};
 
-  const retrieveMessage = (receiverClass) => {
-    setMessageList([]);
-    var requestOptions = {
-      method: "GET",
-      headers: myHeaders,
-      redirect: "follow",
-    };
+	const retrieveMessage = (receiverClass) => {
+		// setMessageList([]);
+		var requestOptions = {
+			method: 'GET',
+			headers: myHeaders,
+			redirect: 'follow',
+		};
 
-    // receiver_id: check id from recently DMs
-    // NOTE: receiver_id can also be the sender of the message to our user
-    // check the result.data.forEach(item.[sender/receiver].uid) to see who is sender/receiver
+		// receiver_id: check id from recently DMs
+		// NOTE: receiver_id can also be the sender of the message to our user
+		// check the result.data.forEach(item.[sender/receiver].uid) to see who is sender/receiver
 
-    // receiver_class: [Channel/User]
-    fetch(
-      `${url}/messages?receiver_class=${receiverClass}&receiver_id=${id}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => {
-        // console.log(`Message retrieved (${receiverClass} ${id}):`, result);
-        let updatedList = [];
-        result.data.forEach((item) => {
-          updatedList.push({
-            body: item.body,
-            created_at: new Date(item.created_at),
-            receiver: item.receiver.uid,
-            sender: item.sender.uid,
-          });
-        });
-        updatedList.forEach((item) => {
-          item.created_at = item.created_at.toUTCString();
-        });
+		// receiver_class: [Channel/User]
+		fetch(
+			`${url}/messages?receiver_class=${receiverClass}&receiver_id=${id}`,
+			requestOptions
+		)
+			.then((response) => response.json())
+			.then((result) => {
+				// console.log(`Message retrieved (${receiverClass} ${id}):`, result);
+				let updatedList = [];
+				result.data.forEach((item) => {
+					updatedList.push({
+						body: item.body,
+						created_at: new Date(item.created_at),
+						receiver: item.receiver.uid,
+						sender: item.sender.uid,
+					});
+				});
+				updatedList.forEach((item) => {
+					item.created_at = item.created_at.toUTCString();
+				});
 
-        // let user = usersList.find(
-        // 	(item) => item.uid === localStorage.getItem('uid')
-        // );
+				// let user = usersList.find(
+				// 	(item) => item.uid === localStorage.getItem('uid')
+				// );
 
-        // if (path === 'messages' && String(id) === String(user.id)) {
-        // 	console.log(updatedList);
-        // 	let ownDM = [];
-        // 	for (let i = 0; i < updatedList.length; i++) {
-        // 		if (i % 2 === 0) ownDM.push(updatedList[i]);
-        // 	}
-        // 	console.log(ownDM);
-        // 	return setMessageList(ownDM);
-        // }
+				// if (path === 'messages' && String(id) === String(user.id)) {
+				// 	console.log(updatedList);
+				// 	let ownDM = [];
+				// 	for (let i = 0; i < updatedList.length; i++) {
+				// 		if (i % 2 === 0) ownDM.push(updatedList[i]);
+				// 	}
+				// 	console.log(ownDM);
+				// 	return setMessageList(ownDM);
+				// }
 
-        setMessageList(updatedList);
-      })
-      .catch((error) => console.log("error", error));
-  };
+				setMessageList(updatedList);
+			})
+			.catch((error) => console.log('error', error));
+	};
 
-  useEffect(() => {
-    getChatDisplay();
-  }, [channelsList, DMList, path, id]); // eslint-disable-line react-hooks/exhaustive-deps
+	useEffect(() => {
+		getChatDisplay();
+	}, [channelsList, DMList, path, id]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  useEffect(() => {
-    getChannelDetails();
-  }, [path, id, newMember]); // eslint-disable-line react-hooks/exhaustive-deps
+	useEffect(() => {
+		getChannelDetails();
+	}, [path, id, newMember]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  useEffect(() => {
-    switch (path) {
-      case "channel":
-        retrieveMessage("Channel");
-        break;
-      case "messages":
-        retrieveMessage("User");
-        break;
-      default:
-    }
-  }, [path, id]); // eslint-disable-line react-hooks/exhaustive-deps
+	useEffect(() => {
+		switch (path) {
+			case 'channel':
+				retrieveMessage('Channel');
+				break;
+			case 'messages':
+				retrieveMessage('User');
+				break;
+			default:
+		}
+	}, [path, id]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  return (
-    <Container>
-      <Header>
-        <Channel>
-          <ChannelName>
-            {path === "channel" ? "#" + display.name : display.uid}
-          </ChannelName>
-          <ChannelInfo>
-            {/* <box-icon name='plus' color='var(--channelinfo-color)' size='20px'></box-icon>
+	return (
+		<Container>
+			<Header>
+				<Channel>
+					<ChannelName>
+						{path === 'channel' ? '#' + display.name : display.uid}
+					</ChannelName>
+					<ChannelInfo>
+						{/* <box-icon name='plus' color='var(--channelinfo-color)' size='20px'></box-icon>
                         <span>Add a bookmark</span> */}
-            {path === "channel" &&
-              (channelMembers.length > 1
-                ? channelMembers.length + " members"
-                : channelMembers.length + " member")}
+						{path === 'channel' &&
+							(channelMembers.length > 1
+								? channelMembers.length + ' members'
+								: channelMembers.length + ' member')}
 
-            {path === "messages" && "Updated on " + display.updated_at}
-          </ChannelInfo>
-        </Channel>
-        <ChannelDetails>
-          {/* <div>
+						{path === 'messages' && 'Updated on ' + display.updated_at}
+					</ChannelInfo>
+				</Channel>
+				<ChannelDetails>
+					{/* <div>
 						{path === 'messages' ? (
 							<span>Details</span>
 						) : (
 							<span>Add member</span>
 						)}
 					</div> */}
-          {path === "messages" ? (
-            // <Info />
-            <box-icon
-              name="info-circle"
-              color="var(--sidebar-font-color)"
-            ></box-icon>
-          ) : (
-            // <Add onClick={() => addMember(id)} />
-            <box-icon
-              name="user-plus"
-              color="var(--sidebar-font-color)"
-              onClick={() => addMember(id)}
-            ></box-icon>
-          )}
-        </ChannelDetails>
-      </Header>
-      <MessageContainer>
-        {messageList.map((item, index) => (
-          <ChatMessage
-            key={index}
-            sender={item.sender}
-            body={item.body}
-            date={item.created_at}
-          />
-        ))}
-      </MessageContainer>
-      <ChatInput
-        onSubmit={sendMessage}
-        onClick={sendMessage}
-        message={message}
-        onChange={inputMessage}
-      />
-    </Container>
-  );
+					{path === 'messages' ? (
+						// <Info />
+						<box-icon
+							name='info-circle'
+							color='var(--sidebar-font-color)'
+						></box-icon>
+					) : (
+						// <Add onClick={() => addMember(id)} />
+						<box-icon	
+							name='user-plus'
+							color='var(--sidebar-font-color)'
+							onClick={() => addMember(id)}
+						></box-icon>
+					)}
+				</ChannelDetails>
+			</Header>
+			<MessageContainer>
+				{messageList.map((item, index) => (
+					<ChatMessage
+						key={index}
+						sender={item.sender}
+						body={item.body}
+						date={item.created_at}
+					/>
+				))}
+			</MessageContainer>
+			<ChatInput
+				onSubmit={sendMessage}
+				onClick={sendMessage}
+				message={message}
+				onChange={inputMessage}
+			/>
+		</Container>
+	);
 };
 
 export default Chat;
