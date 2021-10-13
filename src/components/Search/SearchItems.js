515d5b2b9833e3ab5d2ea9d891b4e886efbdc59c
jerco2 <jericogodoy7296@gmail.com>
2021-10-13 19:08:36 +0800

    Mainpage #16: added search feature display results

---
+++ src/components/Search/SearchItems.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+function SearchItems({ inputAutoSelect, item, setSearchInput }) {
+  const clicked = () => {
+    setSearchInput(item);
+    inputAutoSelect();
+  };
+
+  return (
+    <Container onClick={clicked}>
+      <p>
+        <b>{item}</b>
+      </p>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  cursor: pointer;
+  padding-left: 5px;
+
+  :hover {
+    background-color: #ccc;
+  }
+`;
+export default SearchItems;
