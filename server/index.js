d264ae5ec7d389c231befbc8aefdbf3c2adea678
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2022-01-30 19:15:10 +0800

    modified server (index.js, package.json)

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const router = require('./router');
+// const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 8900;
+
 const app = express();
+const server = http.createServer(app);
+// const io = socketio(server);
+
+server.listen(PORT, () => console.log(`Server has started on PORT ${PORT}`));
+
+const router = require('./router');
 app.use(router);
 
-const io = require('socket.io')(8900, {
+const io = require('socket.io')(server, {
 	cors: {
 		origin: 'http://localhost:3000',
+		methods: ['GET', 'POST'],
 	},
 });
 
